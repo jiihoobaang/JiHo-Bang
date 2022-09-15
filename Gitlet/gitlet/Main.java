05727fdac6ba7b02ae25373ab266a9d946f43e7c
JiHo <jihobang@berkeley.edu>
2022-09-14 17:21:48 -0700

    Projects

--- Gitlet/gitlet/Main.java
+++ Gitlet/gitlet/Main.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package gitlet;
 
 /** Driver class for Gitlet, a subset of the Git version-control system.
@@ -68,29 +67,3 @@ public class Main {
         System.exit(0);
     }
 }
-=======
-package gitlet;
-
-/** Driver class for Gitlet, a subset of the Git version-control system.
- *  @author TODO
- */
-public class Main {
-
-    /** Usage: java gitlet.Main ARGS, where ARGS contains
-     *  <COMMAND> <OPERAND1> <OPERAND2> ... 
-     */
-    public static void main(String[] args) {
-        // TODO: what if args is empty?
-        String firstArg = args[0];
-        switch(firstArg) {
-            case "init":
-                // TODO: handle the `init` command
-                break;
-            case "add":
-                // TODO: handle the `add [filename]` command
-                break;
-            // TODO: FILL THE REST IN
-        }
-    }
-}
->>>>>>> 25b4d4593f721c34e8d02038e24c1a213feffb63
--- Gitlet/gitlet/Repository.java
+++ Gitlet/gitlet/Repository.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package gitlet;
 
 import java.io.File;
@@ -696,34 +695,3 @@ public class Repository implements Serializable {
         return Utils.readObject(com, Commit.class);
     }
 }
-=======
-package gitlet;
-
-import java.io.File;
-import static gitlet.Utils.*;
-
-// TODO: any imports you need here
-
-/** Represents a gitlet repository.
- *  TODO: It's a good idea to give a description here of what else this Class
- *  does at a high level.
- *
- *  @author TODO
- */
-public class Repository {
-    /**
-     * TODO: add instance variables here.
-     *
-     * List all instance variables of the Repository class here with a useful
-     * comment above them describing what that variable represents and how that
-     * variable is used. We've provided two examples for you.
-     */
-
-    /** The current working directory. */
-    public static final File CWD = new File(System.getProperty("user.dir"));
-    /** The .gitlet directory. */
-    public static final File GITLET_DIR = join(CWD, ".gitlet");
-
-    /* TODO: fill in the rest of this class. */
-}
->>>>>>> 25b4d4593f721c34e8d02038e24c1a213feffb63
